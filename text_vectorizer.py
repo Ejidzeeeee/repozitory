d7f1118a6c0441c4c9e24266dfe94074041744d5
Ejidzeeeee <masha.k2604@gmail.com>
2026-06-18 22:53:22 +0300

    Update text_vectorizer.py

--- text_vectorizer.py
+++ text_vectorizer.py
@@ -1,1 +1,16 @@
+"""
+Векторизация текста: TF-IDF, CountVectorizer
+"""
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
+def vectorize_tfidf(texts, max_features=None):
+    """TF-IDF векторизация."""
+    vec = TfidfVectorizer(max_features=max_features)
+    X = vec.fit_transform(texts)
+    return X, vec
+
+def vectorize_count(texts, max_features=None):
+    """CountVectorizer (мешок слов)."""
+    vec = CountVectorizer(max_features=max_features)
+    X = vec.fit_transform(texts)
+    return X, vec
